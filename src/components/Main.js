e8bc95941a487b0bd92a0c16cb19a231e2aa8374
Lucam <luca.mella@studio.unibo.it>
2020-08-18 23:36:22 +0200

    metatag

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,11 +5,19 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import Dashboard from './Dashboard'
 import DataTable from './DataTable'
 
+import MetaTags from 'react-meta-tags';
+
 class Main extends Component {
 
   render() {
     return (
       <div className="main-panel">
+        <MetaTags>
+            <title>DoubleExtortion Tracker</title>
+            <meta id="meta-description" name="description" content="Tracking ransomware events in the wild" />
+            <meta id="og-title" property="og:title" content="DoubleExtortion Tracker" />
+            <meta id="og-image" property="og:image" content="public/screen.jpg" />
+          </MetaTags>
         <Navbar />
         <Switch>
           <Route path="/dashboard" component={Dashboard} />
