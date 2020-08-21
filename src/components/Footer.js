2067a6a2b01fe2ad023fc609f4581e573577cac3
Lucam <luca.mella@studio.unibo.it>
2020-08-21 13:42:29 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import IconButton from '@material-ui/core/IconButton';
+
+import Icon from '@material-ui/core/Icon';
+
 class Footer extends Component {
   render() {
     return (
       <footer className="footer">
+
         <div className="container-fluid">
+        
           <nav>
             <ul className="footer-menu">
               <li>
               </li>
-
             </ul>
+            <p classname="copyright" style={{'textAlign':'center'}}>
+            <IconButton href="https://github.com/luca-m/" target="_blank" >
+              <Icon className="fa fa-github" />
+            </IconButton>
+            <IconButton href="https://www.linkedin.com/in/allemco/" target="_blank" >
+              <Icon className="fa fa-linkedin" />
+            </IconButton>
+            </p>
             <p className="copyright text-center">
-              DoubleExtortion - Ransomware and Breach Tracker - 2020
+               DoubleExtortion - Ransomware and Breach Tracker - 2020
           </p>
           </nav>
         </div>
